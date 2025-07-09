5525031faf97c4df493b155008330137aa3679b9
adamcaak <ladamczak1997@gmail.com>
2025-07-10 00:09:26 +0200

    Create SampleData

---
+++ FriendsFavoriteMovies/SampleData.swift
@@ -0,0 +1,34 @@
+//
+//  SampleData.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Łukasz Adamczak on 09/07/2025.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+class SampleData {
+    static let shared = SampleData()
+    
+    let modelContainer: ModelContainer
+    
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    private init() {
+        let schema = Schema([
+            Friend.self,
+            Movies.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+}
