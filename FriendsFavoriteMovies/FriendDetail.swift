61bf93c074e69f2650e961d8d42e2d8ad5395723
adamcaak <ladamczak1997@gmail.com>
2025-07-11 23:10:18 +0200

    Create FriendDetail

---
+++ FriendsFavoriteMovies/FriendDetail.swift
@@ -0,0 +1,20 @@
+//
+//  FriendDetail.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Łukasz Adamczak on 11/07/2025.
+//
+
+import SwiftUI
+
+struct FriendDetail: View {
+    @Bindable var friend: [Friend]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendDetail()
+}
