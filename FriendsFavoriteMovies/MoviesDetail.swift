165d38702f6b0d65d8dd4f4fa6d9c1d567ca11ab
adamcaak <ladamczak1997@gmail.com>
2025-07-12 23:29:54 +0200

    Update MoviesDetail

--- FriendsFavoriteMovies/MoviesDetail.swift
+++ FriendsFavoriteMovies/MoviesDetail.swift
@@ -9,27 +9,36 @@ import SwiftUI
 
 struct MoviesDetail: View {
     @Bindable var movies: Movies
+    let isNew: Bool
+    
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var contenxt
     
+    init(movies: Movies, isNew: Bool = false) {
+        self.movies = movies
+        self.isNew = isNew
+    }
     var body: some View {
         Form {
             TextField("Name:", text: $movies.title)
                 .autocorrectionDisabled()
             DatePicker("Release Date", selection: $movies.releaseDate)
         }
-        .navigationTitle("Movie")
+        .navigationTitle(isNew ? "New Movie" : "Movie")
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
-            ToolbarItem(placement: .confirmationAction) {
-                Button("Save") {
-                    dismiss()
+            if isNew {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        dismiss()
+                    }
                 }
-            }
-            ToolbarItem(placement: .cancellationAction) {
-                Button("Cancel") {
-                    contenxt.delete(movies)
-                    dismiss()
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        contenxt.delete(movies)
+                        dismiss()
+                    }
                 }
             }
         }
@@ -39,5 +48,11 @@ struct MoviesDetail: View {
 #Preview {
     NavigationStack {
         MoviesDetail(movies: SampleData.shared.movies)
+    }
+}
+
+#Preview("New Movie") {
+    NavigationStack {
+        MoviesDetail(movies: SampleData.shared.movies, isNew: true)
     }
 }
