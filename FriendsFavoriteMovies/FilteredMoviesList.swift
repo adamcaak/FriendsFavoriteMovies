b0e1dc938bd55eb56dc5f023e99caf0aa7f2c244
adamcaak <ladamczak1997@gmail.com>
2025-07-15 22:39:29 +0200

    Create FilteredMoviesList

---
+++ FriendsFavoriteMovies/FilteredMoviesList.swift
@@ -0,0 +1,18 @@
+//
+//  FilteredMoviesList.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Łukasz Adamczak on 15/07/2025.
+//
+
+import SwiftUI
+
+struct FilteredMoviesList: View {
+    var body: some View {
+        MoviesList()
+    }
+}
+
+#Preview {
+    FilteredMoviesList()
+}
